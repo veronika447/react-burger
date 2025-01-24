321124476511c3dc62caf3a52ed886c323b72e35
veronika447 <starenkaya.veronika@gmail.com>
2025-01-24 23:04:34 +0300

    refactor: add types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,12 @@
+import type { RootState, AppDispatch } from "../components/app/store";
+
+export type ThunkAPI = {
+  state: RootState;
+  dispatch: AppDispatch;
+  rejectValue: string;
+  extra: { s: string; n: number };
+};
+
 export type IngredientType = {
   _id: string;
   name: string;
@@ -15,5 +24,53 @@ export type IngredientType = {
 };
 
 export type User = {
-  [key: string]: string;
+  name: string;
+  email: string;
+};
+
+type Response = {
+  success: boolean;
+};
+
+export type LoginRes = Response & {
+  accessToken: string;
+  refreshToken: string;
+  user: User;
+};
+
+export type ChangeUserDataRes = Response & {
+  user: User;
+};
+
+export type CheckEmailRes = Response & {
+  message: string;
+};
+
+export type LogoutRes = Response & {
+  message: string;
+};
+
+export type OrderRes = Response & {
+  order: {
+    number: number;
+  };
+};
+
+export type RefreshTokenRes = Response & {
+  accessToken: string;
+  refreshToken: string;
+};
+
+export type RegisterRes = Response & {
+  accessToken: string;
+  refreshToken: string;
+  user: User;
+};
+
+export type ResetPasswordRes = Response & {
+  message: string;
+};
+
+export type GetIngredientsRes = Response & {
+  data: IngredientType[];
 };
