029672bad5ad9d54f06793459f9ef5a395866320
veronika447 <starenkaya.veronika@gmail.com>
2025-01-24 23:04:11 +0300

    refactor: update redux hooks, add some types for dispatch

--- src/components/app/store.ts
+++ src/components/app/store.ts
@@ -1,4 +1,4 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { AnyAction, combineReducers, configureStore, ThunkDispatch, UnknownAction } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
@@ -12,7 +12,6 @@ import registerFormReducer from "../../services/register-form-slice";
 import forgotPasswordFormReducer from "../../services/forgot-password-form-slice";
 import resetPasswordFormReducer from "../../services/reset-password-form-slice";
 import authReducer from "../../services/auth-slice";
-import { useDispatch, useSelector } from "react-redux";
 
 const persistConfig = {
   key: "root",
@@ -47,7 +46,7 @@ export const store = configureStore({
 
 export const persistor = persistStore(store);
 
-export type RootState = ReturnType<typeof rootReducer>;
-export type AppDispatch = typeof store.dispatch;
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
-export const useAppSelector = useSelector.withTypes<RootState>();
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch & ThunkDispatch<RootState, any, UnknownAction>;
+
+
