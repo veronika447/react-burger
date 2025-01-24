d5378317bcf039e1f2012c51087de7a4afe4a06b
veronika447 <starenkaya.veronika@gmail.com>
2025-01-24 23:05:26 +0300

    refactor: add generic to request function

--- src/utils/check-response.ts
+++ src/utils/check-response.ts
@@ -1,6 +1,6 @@
-export function checkResponse(res: Response) {
+export function checkResponse<T>(res: Response) {
   if (res.ok) {
-    return res.json();
+    return res.json() as T;
   }
   return Promise.reject(res.status);
 }
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,6 +1,9 @@
 import { checkResponse } from "./check-response";
 import { BASE_URL } from "../components/app/app";
 
-export function request(endPoint: string, options?: RequestInit): Promise<any> {
-  return fetch(BASE_URL + endPoint, options).then(checkResponse);
+export async function request<T>(
+  endPoint: string,
+  options?: RequestInit
+): Promise<T> {
+  return fetch(BASE_URL + endPoint, options).then(checkResponse<T>);
 }
