e331d680645df513146985f991f806ced6011bb6
veronika447 <starenkaya.veronika@gmail.com>
2025-02-16 17:11:44 +0300

    refactor: migrate index.jsx to ts

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ import { Provider } from "react-redux";
 import { persistor, store } from "./components/app/store";
 import { PersistGate } from "redux-persist/integration/react";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
